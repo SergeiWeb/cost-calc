f2cc8a4652de1dc01b3577aad941e77cd77c5ae0
Sergei <sergokondrashevskij@ukr.net>
2020-08-15 21:39:23 +0300

    Production version of the app

---
+++ build/precache-manifest.b8a5ace1e66468c0f3c373c62259849f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcb216b21fa6c07096f0fe4b0ee3c7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ce39ddc9b0a458c2fb0",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d79b0460fa7eef72a6f3",
+    "url": "/static/css/main.a7d76489.chunk.css"
+  },
+  {
+    "revision": "2ce39ddc9b0a458c2fb0",
+    "url": "/static/js/2.4a737a53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a737a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79b0460fa7eef72a6f3",
+    "url": "/static/js/main.5b5fd9d1.chunk.js"
+  },
+  {
+    "revision": "c0ccae055cb2c07e921a",
+    "url": "/static/js/runtime-main.4483ebb2.js"
+  },
+  {
+    "revision": "3db31919b255ed7261e7329f0f8303a4",
+    "url": "/static/media/NotoSansJP-Bold.3db31919.otf"
+  },
+  {
+    "revision": "366120c9b4029066302ca6d75afb6f8b",
+    "url": "/static/media/NotoSansJP-Medium.366120c9.otf"
+  },
+  {
+    "revision": "b5f8ef0cb10cf4fa6dfc584d29908421",
+    "url": "/static/media/NotoSansJP-Regular.b5f8ef0c.otf"
+  }
+]);
